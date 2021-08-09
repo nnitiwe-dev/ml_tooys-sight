0270a81b1810d996a0d776f39cf71f8682f1dd26
Samuel Theophilus <samtheo1597@outlook.com>
2021-08-09 19:21:21 +0100

    Fix Error

--- computervision/urls.py
+++ computervision/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index_page, name = 'index'),
-    path('', views.text_2_hand, name = 'text_2_hand'),
+    path('text_to_hand/', views.text_2_hand, name = 'text_2_hand'),
     #path('summarizer/', views.web_summarizer, name = 'summarizer'),
     #path('tweet_sentiments/', views.twitter, name='twitter'),
     #path('names/', views.name_classify, name='nigerian_names'),
