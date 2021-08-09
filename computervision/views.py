0ff7c0e3e6f053c57ddc88b3775b5a63908ef637
Samuel Theophilus <samtheo1597@outlook.com>
2021-08-10 00:53:13 +0100

    Update Views

--- computervision/views.py
+++ computervision/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 from django.conf import settings
 import os
-
-
-import pywhatkit as kit
+import cv_models.super_resolution as sr
 
 
 # Create your views here.
@@ -13,6 +11,15 @@ def index_page(request):
 def super_resolution(request):
 	saved_image=None
 
+
+	lr_image = sr.preprocess_image(IMAGE_PATH)
+
+	model = sr.load_model()
+	start = time.time()
+	gen_image = model(lr_image)
+	gen_image = tf.squeeze(gen_image)
+	print("Time Taken: %f" % (time.time() - start))
+	
 	# if request.GET.get('comment', False) is not False:
 	# 	path = os.path.join(settings.MEDIA_ROOT,'text_2_hand.png')
 	# 	kit.text_to_handwriting(request.GET['comment'],save_to=path)
